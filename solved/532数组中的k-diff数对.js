10278be1a1e18f9052f42727a9f93aabf068c120
Licx <554735719@qq.com>
2022-06-16 01:11:59 +0800

    day16

---
+++ solved/532数组中的k-diff数对.js
@@ -0,0 +1,53 @@
+/**
+给定一个整数数组和一个整数 k，你需要在数组里找到 不同的 k-diff 数对，并返回不同的 k-diff 数对 的数目。
+
+这里将 k-diff 数对定义为一个整数对 (nums[i], nums[j])，并满足下述全部条件：
+
+0 <= i < j < nums.length
+|nums[i] - nums[j]| == k
+注意，|val| 表示 val 的绝对值。
+
+示例 1：
+
+输入：nums = [3, 1, 4, 1, 5], k = 2
+输出：2
+解释：数组中有两个 2-diff 数对, (1, 3) 和 (3, 5)。
+尽管数组中有两个1，但我们只应返回不同的数对的数量。
+示例 2：
+
+输入：nums = [1, 2, 3, 4, 5], k = 1
+输出：4
+解释：数组中有四个 1-diff 数对, (1, 2), (2, 3), (3, 4) 和 (4, 5)。
+示例 3：
+
+输入：nums = [1, 3, 1, 5, 4], k = 0
+输出：1
+解释：数组中只有一个 0-diff 数对，(1, 1)。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/k-diff-pairs-in-an-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findPairs = function (nums, k) {
+	let set = new Set()
+	let res = new Set()
+	for (let i = 0; i < nums.length; i++) {
+		if (set.has(nums[i] - k)) {
+			res.add(nums[i] - k)
+		}
+		if (set.has(k + nums[i])) {
+			res.add(nums[i])
+		}
+		set.add(nums[i])
+	}
+	return res.size
+}
+
+;(nums = [3, 1, 4, 1, 5]), (k = 2)
+console.log(findPairs(nums, k))
